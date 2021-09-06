4c7ca7b6f0847f8c9bf8511077afe499404a8e46
zahraa982 <53355685+zahraa982@users.noreply.github.com>
2021-09-06 15:45:33 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,28 @@
+
+const navTriggerEl = document.querySelector(".hamburger");
+const navEl = document.querySelector("nav");
+const  conentEl = document.querySelector(".content");
+const hamburgerBarsEl=document.getElementsByTagName("span");
+console.log(hamburgerBarsEl) //just to be sure they are there
+
+
+function toggelnav(){
+navTriggerEl.addEventListener("click", function(){
+navEl.classList.toggle("open");
+conentEl.classList.toggle("shift");
+animateHhamburgers();
+
+});
+
+}
+
+function animateHhamburgers(){
+for (let item of hamburgerBarsEl){
+item.classList.toggle("change");
+
+}
+
+}
+
+toggelnav();
+
